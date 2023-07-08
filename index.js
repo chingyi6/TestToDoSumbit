b0512bd4cbedc99e751544bf509cf61cffd8b6ae
chingyi6 <134117853+chingyi6@users.noreply.github.com>
2023-07-08 12:17:45 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+var getRawBody = require('raw-body');
+var getFormBody = require('body/form');
+var body = require('body');
+const r = require('./route')
+
+
+module.exports.handler = function(req, resp, context) {
+    console.log('hello world');
+/*
+    var params = {
+        path: req.path,
+        queries: req.queries,
+        headers: req.headers,
+        method : req.method,
+        requestURI : req.url,
+        clientIP : req.clientIP,
+    }
+    */
+        
+    getRawBody(req, function(err, body) {
+        resp.setHeader('content-type', 'application/json');
+
+        var uri = (req.url).split('/');
+        if(uri.length == 0){
+            resp.send(JSON.stringify({'code': 400, 'body': 'Bad Request' }))
+        } else {
+            var route = uri[uri.length - 1]
+            console.log(route)
+            switch(req.method){
+                case 'GET':
+                    r.get(route, JSON.parse(body.toString()))
+                    .then((c)=>{
+                        resp.send(JSON.stringify(c, null, ''))
+                    })
+                    break;
+
+                case 'POST':
+                    r.post(route, JSON.parse(body.toString()))
+                    .then((c)=>{
+                        resp.send(JSON.stringify(c, null, ''))
+                    })
+                    break;
+                    
+                case 'PUT':
+                    r.put(route, JSON.parse(body.toString()))
+                    .then((c)=>{
+                        resp.send(JSON.stringify(c, null, ''))
+                    })
+
+                default:
+                    resp.send(JSON.stringify({ 'code': 400, 'body': 'Bad Request'}, null, ''))
+            }
+        }
+    });   
+}
